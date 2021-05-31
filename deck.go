a47f606a42fb5a5f17746bcb158820e8942fba9d
Kaushal Kumar Singh <kaushal.singh@redbus.com>
2021-05-31 16:58:17 +0530

    intial code

---
+++ deck.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// create a new type of "deck"
+// which is a slice of string
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Diamonds", "Spade", "Hearts", "Club"}
+	cardValues := []string{"Ace", "two", "three", "four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for index, card := range d {
+		fmt.Println(index, card)
+	}
+}
