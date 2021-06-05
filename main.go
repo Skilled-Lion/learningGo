78e6de12db18cdcbc8e3e2217dd6666b73351c9e
Kaushal Kumar Singh <kaushal.singh@redbus.com>
2021-06-05 09:47:03 +0530

    intial struct

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
 func main() {
-	cards := newDeck()
-	cards.shuffle()
-	fmt.Println(cards)
+	var kaushal person
+	kaushal.firstName = "kaushal"
+	kaushal.lastName = "singh"
+	alex := person{firstName: "alex", lastName: "anderson"}
+	fmt.Println(alex)
+	fmt.Println(kaushal)
+	fmt.Printf("%+v", alex)
 }
